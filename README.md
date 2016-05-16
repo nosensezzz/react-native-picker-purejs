332093f00f04899c6300096f4c57139c6f5f145c
3z <353360104@qq.com>
2016-05-15 22:26:03 -0400

    v1.1.4

---
+++ README.md
@@ -0,0 +1,84 @@
+# react-native-picker-purejs
+npm module - react native picker 
+
+## how to install
+under your rn project folder path
+run
+npm install react-native-picker-purejs --save
+
+## Description
+Easy to use, free to use.
+
+## Props
+# _ref
+this one is the referance name to your component, all picker component will listen to same event, you have to use _ref to decide which component your are using.
+
+# offsetTop
+Default the picker component will have a height of 1/3 screen height, and placed at 2/3 top and 0 left, so it will cover the bottom 1/3 of the screen.
+In case your view component is not start from the very top, so you can use offsetTop property to adjust the position of the picker component.
+
+# options
+Options values...
+
+# value
+default value...
+
+# event
+must have property to support the component work, it's a instance of EventEmitter, and your listeners should use the same instance too. 
+
+Here is a example of how to use it.
+
+In your RN projects.
+
+    EventEmitter = require('EventEmitter'),
+    NPMPICKER = require("react-native-picker-purejs"),
+
+    componentWillMount(){
+    	this.eventEmitter = new EventEmitter();
+    }
+
+    componentDidMount(){
+    	var self = this;
+    	self.eventEmitter.addListener('pickerEventDone', this.pickerEventDone.bind(self));
+    	self.eventEmitter.addListener('pickerEventCancel', this.pickerEventCancel.bind(self));
+    	self.eventEmitter.addListener('pickerEventValueChanged', this.pickerEventValueChanged.bind(self));
+    }
+
+    toggle(){
+    	var self = this;
+        if(ref == self.props._ref){
+            this.setState({display: !self.state.display});
+        }
+    }
+
+    pickerEventCancel(){
+      // cancel button clicked
+    	console.log(0);
+    }
+
+    pickerEventValueChanged(param){
+      // value changed 
+    	console.log(param);
+    }
+
+    pickerEventDone(param){
+      // done button clicked.
+    	console.log(param);
+    }
+    return (  
+    ...
+    ...
+    <NPMPICKER
+    _ref={"test"}
+    offsetTop={60}
+    options={["1", "2", "3"]}
+    value={"1"}
+    event={self.eventEmitter} /> );
+   
+			
+## About the styles
+Put the PickerComponent at your out most ViewComponent, it default sets to 1/3 height and touch the bottom, but you can always set yourself include the code, if you work on rn project, it will be very easy to set up it.
+
+## Screen shot
+
+https://cloud.githubusercontent.com/assets/1654228/15271062/b779b7ac-1a04-11e6-928a-c3e7686b186a.png
--- main.js
+++ main.js
@@ -26,173 +26,175 @@ class PickerComponent extends Component {
         super(props);
         var self = this;
         this.state = {
-        	defaultHeight: screenHeight/3,
-        	defaultWidth: screenWidth,
+            defaultHeight: screenHeight/3,
+            defaultWidth: screenWidth,
+            offsetTop: props.offsetTop,
 
-        	display: false,
-        	user: props.user,
-        	key: props.key,
-        	value: props.value,
-        	options: props.options,
+            display: false,
+            key: props.key,
+            value: props.value,
+            options: props.options,
 
-        	buttonCancelColor: "#008CBA",
-        	buttonOkColor: "#008CBA"
+            buttonCancelColor: "#008CBA",
+            buttonOkColor: "#008CBA"
         };
     }
 
     componentDidMount(){
-    	var self = this;
-    	this.props.event.addListener('toggle', this.toggle.bind(self));
+        var self = this;
+        this.props.event.addListener('toggle', this.toggle.bind(self));
     }
 
-    toggle(){
-    	var self = this;
-    	this.setState({display: !self.state.display});
+    toggle(ref){
+        var self = this;
+        if(ref == self.props._ref){
+            this.setState({display: !self.state.display});
+        }
     }
 
     cancel(){
-    	this.setState({display: false});
-    	this.props.event.emit("pickerEventCancel");
+        this.setState({display: false});
+        this.props.event.emit("pickerEventCancel");
     }
 
     done(){
-    	var self = this;
-    	this.setState({display: false});
-    	this.props.event.emit("pickerEventDone" , {
-    		ref: self.props._ref,
-    		value: self.state.value
-    	});
+        var self = this;
+        this.setState({display: false});
+        this.props.event.emit("pickerEventDone" , {
+            ref: self.props._ref,
+            value: self.state.value
+        });
     }
 
     onValueChange(val){
-    	var self = this;
-    	self.setState({value: val});
-    	this.props.event.emit("pickerEventValueChanged" , {
-    		ref: self.props._ref,
-    		value: val
-    	});
+        var self = this;
+        self.setState({value: val});
+        this.props.event.emit("pickerEventValueChanged" , {
+            ref: self.props._ref,
+            value: val
+        });
     }
 
     getPicker(){
-    	var 
-    	self = this,
-    	PickerItems = [];
+        var 
+        self = this,
+        PickerItems = [];
 
-    	_.forEach(self.state.options, function (option, n) {
+        _.forEach(self.state.options, function (option, n) {
 
-    		PickerItems.push(
-    			<Picker.Item label={option} value={option} key={n}/>
-    		);
-    	});
+            PickerItems.push(
+                <Picker.Item label={option} value={option} key={n}/>
+            );
+        });
 
-    	return (
-    		<View style={{
+        return (
+            <View style={{
 
-    		}}>
-	    		<Picker
-				  selectedValue={self.state.value}
-				  onValueChange={self.onValueChange.bind(self)}>
-				  	{PickerItems}
-				</Picker>
-			</View>);
+            }}>
+                <Picker
+                  selectedValue={self.state.value}
+                  onValueChange={self.onValueChange.bind(self)}>
+                    {PickerItems}
+                </Picker>
+            </View>);
     }
 
     render() {
-    	var 
-    	self = this,
-    	height, width,
-    	content, picker,
-    	picker;
-    	height = self.state.display? self.state.defaultHeight : 0;
-    	width = self.state.display? self.state.defaultWidth : 0;
+        var 
+        self = this,
+        height, width,
+        content, picker,
+        picker;
+        height = self.state.display? self.state.defaultHeight : 0;
+        width = self.state.display? self.state.defaultWidth : 0;
 
-    	picker = self.getPicker();
+        picker = self.getPicker();
 
-    	if(self.state.display){
-    		content = 
-    			<View style={{
-    				flex: 1,
-    				position: "relative"
-    			}}>
-    				<View style={{
-    					position: "absolute",
-    					top: 0,
-    					left: 0,
-    					height: ActionRowHeight,
-    					padding: 5,
-    					width: screenWidth,
-    					backgroundColor: "#444444",
-    					justifyContent: "center",
-    					flexDirection: "row"
-    				}}>
-    					<View 
-    					onTouchStart={()=>{ 
-    						self.setState({buttonCancelColor: "#005875"}); 
-    					}}
-    					onTouchEnd={()=>{
-    						self.setState({buttonCancelColor: "#008CBA"}); 
-    						self.cancel();
-    					}}
-    					style={[styles.PickerButton, {backgroundColor: self.state.buttonCancelColor}]}>
-	    					<Text style={{
-	    						color: "white",
-	    						alignSelf: "center",
-	    						fontSize: 20
-	    					}}>Cancel</Text>
-    					</View>
-    					<View style={{
-    						flexDirection: "column",
-	    					height: ActionRowHeight - 10,
-	    					width: screenWidth/2,
-	    					justifyContent: "center"
-	    				}}></View>
+        if(self.state.display){
+            content = 
+                <View style={{
+                    flex: 1,
+                    position: "relative"
+                }}>
+                    <View style={{
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        height: ActionRowHeight,
+                        padding: 5,
+                        width: screenWidth,
+                        backgroundColor: "#444444",
+                        justifyContent: "center",
+                        flexDirection: "row"
+                    }}>
+                        <View 
+                        onTouchStart={()=>{ 
+                            self.setState({buttonCancelColor: "#005875"}); 
+                        }}
+                        onTouchEnd={()=>{
+                            self.setState({buttonCancelColor: "#008CBA"}); 
+                            self.cancel();
+                        }}
+                        style={[styles.PickerButton, {backgroundColor: self.state.buttonCancelColor}]}>
+                            <Text style={{
+                                color: "white",
+                                alignSelf: "center",
+                                fontSize: 20
+                            }}>Cancel</Text>
+                        </View>
+                        <View style={{
+                            flexDirection: "column",
+                            height: ActionRowHeight - 10,
+                            width: screenWidth/2,
+                            justifyContent: "center"
+                        }}></View>
 
-    					<View 
-    					onTouchStart={()=>{ 
-    						self.setState({buttonOkColor: "#005875"}); 
-    					}}
-    					onTouchEnd={()=>{
-    						self.setState({buttonOkColor: "#008CBA"});
-    						self.done(); 
-    					}}
-    					style={[styles.PickerButton, {backgroundColor: self.state.buttonOkColor}]}>
-	    					<Text style={{
-	    						color: "white",
-	    						alignSelf: "center",
-	    						fontSize: 20
-	    					}}>Ok</Text>
-    					</View>
-    				</View>
+                        <View 
+                        onTouchStart={()=>{ 
+                            self.setState({buttonOkColor: "#005875"}); 
+                        }}
+                        onTouchEnd={()=>{
+                            self.setState({buttonOkColor: "#008CBA"});
+                            self.done(); 
+                        }}
+                        style={[styles.PickerButton, {backgroundColor: self.state.buttonOkColor}]}>
+                            <Text style={{
+                                color: "white",
+                                alignSelf: "center",
+                                fontSize: 20
+                            }}>Ok</Text>
+                        </View>
+                    </View>
 
-    				<View style={{
-    					position: "absolute",
-    					top: ActionRowHeight,
-    					left: 0,
-    					height: screenHeight/3 - ActionRowHeight,
-    					padding: 0,
-    					width: screenWidth,
-    					//backgroundColor: "#000000",
-    					// justifyContent: "center",
-    					// flexDirection: "row"
-    				}}>
-    					{picker}
-    				</View>
-    			</View>;
-    	} else {
-    		content = null;
-    	}
+                    <View style={{
+                        position: "absolute",
+                        top: ActionRowHeight,
+                        left: 0,
+                        height: screenHeight/3 - ActionRowHeight,
+                        padding: 0,
+                        width: screenWidth,
+                        //backgroundColor: "#000000",
+                        // justifyContent: "center",
+                        // flexDirection: "row"
+                    }}>
+                        {picker}
+                    </View>
+                </View>;
+        } else {
+            content = null;
+        }
 
         return (
-        	<View style={{
-    			position: "absolute",
-    			width: width,
-    			height: height,
-    			top: screenHeight*2/3,
-    			left: 0,
-    			backgroundColor: "#e7e7e7"
-    		}}>
-    			{content}
-        	</View>
+            <View style={{
+                position: "absolute",
+                width: width,
+                height: height,
+                top: screenHeight*2/3 - self.state.offsetTop,
+                left: 0,
+                backgroundColor: "#e7e7e7"
+            }}>
+                {content}
+            </View>
         );
     }
 }
@@ -200,12 +202,12 @@ class PickerComponent extends Component {
 var styles = StyleSheet.create({
 // button clicked color #3e8e41
 PickerButton: {
-	flexDirection: "column",
-	borderRadius: 5,
-	marginLeft: 5,
-	height: ActionRowHeight - 10,
-	width: screenWidth/4-10,
-	justifyContent: "center"
+    flexDirection: "column",
+    borderRadius: 5,
+    marginLeft: 5,
+    height: ActionRowHeight - 10,
+    width: screenWidth/4-10,
+    justifyContent: "center"
 }
 });
 
